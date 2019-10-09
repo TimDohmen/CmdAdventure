c7eea0ae30ad1af52daa59de69d0b1f3fe95e4ed
Tim Dohmen <dohmentim@yahoo.com>
2019-10-09 16:15:04 -0600

    can use look

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -2,15 +2,28 @@ using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Game : IGame
-    {
-        public IRoom CurrentRoom { get; set; }
-        public IPlayer CurrentPlayer { get; set; }
+  public class Game : IGame
+  {
+    public Room CurrentRoom { get; set; }
+    public IPlayer CurrentPlayer { get; set; }
 
-        //NOTE Make yo rooms here...
-        public void Setup()
-        {
-            throw new System.NotImplementedException();
-        }
+    //NOTE Make yo rooms here...
+    public void Setup()
+    {
+      Room start = new Room("First Room", "Dim lit room not a lot going on here");
+      Room two = new Room("Second Room", "Bright Room too bright to see");
+
+      start.AddRoomConnection(two);
+      two.AddRoomConnection(start);
+
+      Item sword = new Item("Rusty Sword", "Big long sword");
+      start.Items.Add(sword);
+      CurrentRoom = start;
     }
+    public Game()
+    {
+      CurrentPlayer = new Player();
+      Setup();
+    }
+  }
 }
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -3,11 +3,51 @@ using ConsoleAdventure.Project.Interfaces;
 
 namespace ConsoleAdventure.Project.Models
 {
-    public class Room : IRoom
+  public class Room : IRoom
+  {
+
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Item> Items { get; set; }
+
+    public Dictionary<string, IRoom> Exits { get; set; }
+
+    public void AddRoomConnection(IRoom room)
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Item> Items { get; set; }
-        public Dictionary<string, IRoom> Exits { get; set; }
+      Exits.Add(room.Name, room);
     }
+
+    public IRoom Move(string direction)
+    {
+      if (Exits.ContainsKey(direction))
+      {
+        return Exits[direction];
+      }
+      return this;
+    }
+
+    public string GetTemplate()
+    {
+      return $@"
+      
+You are now in {Name}
+ {Description}
+ 
+ In this room you find:
+ {Items}
+
+ You also have
+ {Exits}
+ ";
+    }
+    public Room(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      Items = new List<Item>();
+      Exits = new Dictionary<string, IRoom>();
+    }
+  }
+
 }
